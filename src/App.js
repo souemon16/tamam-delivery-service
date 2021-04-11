e6808714d229a8530141284eed84e7cedb9f0fb4
souemon16 <souemon16@gmail.com>
2021-04-12 04:44:44 +0600

    some changes done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
-import React from 'react';
-import Homepage from './Components/Home/Homepage';
-import AboutUs from './Components/About Us/AboutUs';
-import OurAim from './Components/Our Aim/OurAim';
-import Driver from './Components/Driver/Driver';
-import Partner from './Components/Partner/Partner';
+import React, { Suspense, lazy } from 'react';
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-import NotFound from './Components/NotFound/NotFound';
-import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
-import ContactUs from './Components/Contact Us/ContactUs';
+
+
+// import Homepage from './Components/Home/Homepage';
+const Homepage = lazy(() => import('./Components/Home/Homepage'));
+// import AboutUs from './Components/About Us/AboutUs';
+const AboutUs = lazy(() => import('./Components/About Us/AboutUs'));
+// import OurAim from './Components/Our Aim/OurAim';
+const OurAim = lazy(() => import('./Components/Our Aim/OurAim'));
+// import Driver from './Components/Driver/Driver';
+const Driver = lazy(() => import('./Components/Driver/Driver'));
+// import Partner from './Components/Partner/Partner';
+const Partner = lazy(() => import('./Components/Partner/Partner'));
+// import NotFound from './Components/NotFound/NotFound';
+const NotFound = lazy(() => import('./Components/NotFound/NotFound'));
+// import ContactUs from './Components/Contact Us/ContactUs';
+const ContactUs = lazy(() => import('./Components/Contact Us/ContactUs'));
 
 function App() {
   return (
     <Router>
       <ScrollToTop >
-      <Switch>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route exact path="/about-us">
-          <AboutUs />
-        </Route>
-        <Route exact path="/our-aim">
-          <OurAim />
-        </Route>
-        <Route exact path="/contact-us">
-          <ContactUs />
-        </Route>
-        <Route exact path="/become-a-driver">
-          <Driver />
-        </Route>
-        <Route exact path="/become-a-partner">
-          <Partner />
-        </Route>
-        <Route path="/*">
-          <NotFound />
-        </Route>
-      </Switch>
+        <Suspense fallback={<div className="d-flex justify-content-center align-items-center" style={{height: '100vh'}}>
+          <div className="spinner-grow text-secondary" style={{width: '3rem', height: '3rem' }} role="status"> </div> </div>}>
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/about-us">
+              <AboutUs />
+            </Route>
+            <Route exact path="/our-aim">
+              <OurAim />
+            </Route>
+            <Route exact path="/contact-us">
+              <ContactUs />
+            </Route>
+            <Route exact path="/become-a-driver">
+              <Driver />
+            </Route>
+            <Route exact path="/become-a-partner">
+              <Partner />
+            </Route>
+            <Route path="/*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
       </ScrollToTop>
-    </Router>
+    </Router >
   );
 }
 
